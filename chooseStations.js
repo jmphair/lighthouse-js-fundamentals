ca9580e68edd5f1b33e81d8fc83fa206cfe1ad9b
Jonathan Phair <jmphair@gmail.com>
2022-11-14 15:05:50 -0500

    use push method to create array that prints

---
+++ chooseStations.js
@@ -0,0 +1,21 @@
+function chooseStations (stations) {
+  const goodStations = []
+  for (const station of stations) {
+    const capacity = station[1]
+    if (capacity >= 20) {
+      const type = station[2]
+      if (type === "school" || type === "community centre") {
+        goodStations.push(station[0]);
+      }
+    }
+  }
+  return goodStations;
+}
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+chooseStations(stations);
