8b42f67e5db2ecd1ca8423fcb0e0ab8f61552c68
Jonathan Phair <jmphair@gmail.com>
2022-11-15 16:26:17 -0500

    passed quiz, fairly simple...

---
+++ ice-cream.js
@@ -0,0 +1,25 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+iceCreamFlavours.push ("root beer");
+console.log(iceCreamFlavours);
+
+// First step passed, next code below here...
+
+console.log(iceCreamFlavours[0]);
+
+// Next step passed, next code below here...
+
+console.log(iceCreamFlavours[5]);
+
+// Passed, next code below...
+
+
+
+
+
+
+
