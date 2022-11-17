0a16d07ebcf1e97cf0bd81d83f284d51453789f5
Jonathan Phair <jmphair@gmail.com>
2022-11-17 13:38:26 -0500

    need practice and more knowledge base...

---
+++ concat.js
@@ -0,0 +1,10 @@
+function concat(arrayOne, arrayTwo) {
+  let arrayThree = [];
+  arrayThree = arrayOne.concat(arrayTwo);
+  return arrayThree;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
